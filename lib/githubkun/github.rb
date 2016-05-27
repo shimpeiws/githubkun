85e334cf56a0f7cfaa337d2519fd23a38db4c4f8
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-05-27 20:29:46 +0900

    Implment github class

--- lib/githubkun/github.rb
+++ lib/githubkun/github.rb
@@ -1,11 +1,14 @@
 module Githubkun
   class Github
-    def initialize
+    API_ENDPOINT = "https://api.github.com"
 
+    def initialize(github_token)
+      @github_token = github_token
     end
 
     def repositories
-
+      json = RestClient.get("#{API_ENDPOINT}/user/repos", "Authorization" => "token #{@github_token}", accept: :json).body
+      JSON.parse(json, symbolize_names: true).map { |repository| repository[:name] }
     end
   end
 end
